1d9c394a5d5dc3b2d6f60a4484641dcb0b60a4c2
Ivan Milovanov <85357891+AaronDeluna@users.noreply.github.com>
2025-01-30 15:01:06 +0300

    Add files via upload

---
+++ dz2.sql
@@ -0,0 +1,40 @@
+--Количество заказов за все время
+select count(*) as "Количество заказов"
+from orders;
+
+--Расчет суммы денег за все заказы
+select
+    order_details.order_id,
+    sum((unit_price - (unit_price * discount / 100)) * quantity) AS "Итоговый чек"
+from order_details
+group by order_details.order_id;
+
+--Расчет количество сотрудников по городу
+select city, count(*) "Количество сотрудников"
+from employees
+group by employees.city;
+
+--Сумма всех заказов сотрудника выод: (фио, сумма)
+select 
+	concat(employees.first_name, ' ', employees.last_name) "Фио",
+	sum(unit_price * quantity) "Сумма всех заказов"
+from orders 
+inner join employees on orders.employee_id = employees.employee_id
+inner join order_details on orders.order_id = order_details.order_id
+group by employees.first_name, employees.last_name;
+
+--Перечень товаров от самых продаваемых до самых непродаваемых
+
+select 
+    products.product_name "Наименование товара",
+    sum(order_details.quantity) "Количество штук"
+FROM order_details
+INNER JOIN products ON order_details.product_id = products.product_id
+GROUP BY products.product_name
+ORDER BY total_sold DESC;
+
+
+
+
+
+
